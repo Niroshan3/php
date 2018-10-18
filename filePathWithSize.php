7dc258c48e83c4f7784a71c3f40049f7d4108937
Niroshan <Niroshan3@users.noreply.github.com>
2018-10-18 09:55:47 +0530

    Create filePathWithSize.php

---
+++ filePathWithSize.php
@@ -0,0 +1,48 @@
+<?php
+function getDirContents($dir, &$results = array()){
+    $files = scandir($dir);
+    foreach($files as $key => $value){
+        $path = realpath($dir.DIRECTORY_SEPARATOR.$value);
+        if(!is_dir($path)) {
+            $results[] = ['path'=>$path,'size'=>formatSizeUnits(filesize($path))];
+        } else if($value != "." && $value != "..") {
+            getDirContents($path, $results);
+            $results[] = ['path'=>$path,'size'=>filesize($path)];
+        }
+    }
+    return $results;
+}
+$fileslist = getDirContents('html/media/'); // Enter your path here
+echo "<pre>";
+print_r($fileslist); 
+
+
+function formatSizeUnits($bytes)
+    {
+        if ($bytes >= 1073741824)
+        {
+            $bytes = number_format($bytes / 1073741824, 2) . ' GB';
+        }
+        elseif ($bytes >= 1048576)
+        {
+            $bytes = number_format($bytes / 1048576, 2) . ' MB';
+        }
+        elseif ($bytes >= 1024)
+        {
+            $bytes = number_format($bytes / 1024, 2) . ' KB';
+        }
+        elseif ($bytes > 1)
+        {
+            $bytes = $bytes . ' bytes';
+        }
+        elseif ($bytes == 1)
+        {
+            $bytes = $bytes . ' byte';
+        }
+        else
+        {
+            $bytes = '0 bytes';
+        }
+
+        return $bytes;
+}
